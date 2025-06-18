3deefcafbf8353d16737eaa0b77e28ef1eae8498
dsgn-x-ops <joao.andre.barata98@gmail.com>
2025-06-18 20:12:42 +0100

    feat: migrated project to Gradle Kotlin DSL

---
+++ src/processor/build.gradle.kts
@@ -0,0 +1,45 @@
+plugins {
+    id("org.springframework.boot")
+    id("io.spring.dependency-management")
+    java
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+dependencies {
+    // Spring Boot
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-amqp")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+
+    // Circuit Breaker (Resilience4j)
+    implementation("org.springframework.cloud:spring-cloud-starter-circuitbreaker-resilience4j")
+
+    // Database
+    runtimeOnly("org.postgresql:postgresql")
+
+    // Monitoring
+    implementation("io.micrometer:micrometer-registry-prometheus")
+
+    // Lombok
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+
+    // Tests
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+// Spring Cloud version (equivalente a dependencyManagement)
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:2023.0.0")
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
